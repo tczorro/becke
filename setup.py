af6828b2e48c5e5292f24e4569b293e0d2a7ee70
Derrick <yxt1991@gmail.com>
2019-09-09 18:00:19 -0400

    Add extension to setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
-from setuptools import find_packages, setup, dist
+from setuptools import find_packages, setup, dist, Extension
 
 dist.Distribution().fetch_build_eggs(['Cython>=0.29', 'numpy>=1.16'])
 
 from Cython.Build import cythonize
 import numpy
 
+modules=[Extension('becke.becke', ["src/becke/becke.pyx"])]
+
 setup(
     name="becke",
-    version='0.0.13',
+    version='0.0.14',
     description='Fast and memory efficient implementation of becke weights.',
     auther='Derrick Yang',
     author_email='yxt1991@gmail.com',
     url='https://github.com/tczorro/becke.git',
     package_dir={"": "src"},
-    ext_modules=cythonize(["src/becke/*.pyx"]),
+    ext_modules=cythonize(modules),
     include_dirs=[numpy.get_include()],
     packages=find_packages(),
     zip_safe=False,
